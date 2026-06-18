6ddb38cd5dca77e4c41794993353c376550dcf89
Sergei <52406767+Nomad-lab-sketch@users.noreply.github.com>
2026-06-18 21:31:26 +0300

    тест

--- main.go
+++ main.go
@@ -2,24 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/hashicorp/mdns"
 )
 
+const serviceName = "_foobar._tcp"
+
 func main() {
+	go func() {
+		http.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
+			w.Write([]byte("Hello"))
+		})
 
-	host, error := os.Hostname()
+		log.Println("HTTP server on :8000")
+		log.Fatal(http.ListenAndServe(":8000", nil))
+	}()
 
-	if error == nil {
-		fmt.Printf("Hostname: %s", host)
-		return
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	info := []string{"My awesome service"}
+	service, err := mdns.NewMDNSService(
+		host,
+		serviceName,
+		"",
+		"",
+		8000,
+		nil,
+		info,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	service, _ := mdns.NewMDNSService(host, "_foobar._tcp", "", "", 8000, nil, info)
-
-	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+	server, err := mdns.NewServer(&mdns.Config{Zone: service})
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer server.Shutdown()
+
+	fmt.Println("mDNS service published")
+
+	select {}
 }
